d3ba9f09b62278d2358d90f9d8bf1b6fe8cfb023
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-01-16 12:32:24 +0530

    minor changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/User')
 const router = express.Router()
 
 // Create a user using: POST "/api/auth/"
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
     console.log(req.body)
     const user = User(req.body)
     user.save()
