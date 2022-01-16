18ed5222618e51d88e0e3d8cfd0be61ee1220904
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-01-16 11:54:23 +0530

    routes added

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ const express = require('express')
 connectToMongo();
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(3000, () => {
   console.log(`App listening at http://localhost:3000`)
