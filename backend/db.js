25043085b7bb327f0227e87df4aa44a622562a2d
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-01-16 12:27:44 +0530

    rollback to local mongoDB

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = `mongodb+srv://arsalan:${process.env.REACT_APP_ATLAS_PASS}@cluster0.76fym.mongodb.net/${process.env.REACT_APP_DB_NAME}?retryWrites=true&w=majority`
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, ()=> {
