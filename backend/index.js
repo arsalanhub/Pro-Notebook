180020bbc58761c7032d8f8fb51e7b08711b29bc
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-01-16 11:22:14 +0530

    express added

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 const connectToMongo = require('./db')
+const express = require('express')
 
 connectToMongo();
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+  console.log(`App listening at http://localhost:3000`)
+})
