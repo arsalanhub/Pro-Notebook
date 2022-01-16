fbbd08ffba630f8ccfb2fedc5cde42008273a931
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-01-16 12:15:07 +0530

    saving user in database

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const express = require('express')
 connectToMongo();
 const app = express()
 
+// If you want to use req.body then below code will be required
+app.use(express.json())
+
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
